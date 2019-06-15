c7ac086045a18db86741123719f46c6a06b0697d
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-06-15 14:37:11 +0200

    Updated the auction module

--- x/auction/client/cli/tx.go
+++ x/auction/client/cli/tx.go
@@ -2,25 +2,40 @@ package cli
 
 import (
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/client"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/utils"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtxb "github.com/cosmos/cosmos-sdk/x/auth/client/txbuilder"
+	auth "github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/kava-labs/kava-devnet/blockchain/x/auction"
 	"github.com/spf13/cobra"
 )
 
-// GetCmdPlaceBid cli command for creating and modifying cdps.
-func GetCmdPlaceBid(cdc *codec.Codec) *cobra.Command {
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	auctionTxCmd := &cobra.Command{
+		Use:   "auction",
+		Short: "auction transactions subcommands",
+	}
+
+	auctionTxCmd.AddCommand(client.PostCommands(
+		getCmdPlaceBid(cdc),
+	)...)
+
+	return auctionTxCmd
+}
+
+// getCmdPlaceBid cli command for creating and modifying cdps.
+func getCmdPlaceBid(cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
 		Use:   "placebid [AuctionID] [Bidder] [Bid] [Lot]",
 		Short: "place a bid on an auction",
 		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
-			txBldr := authtxb.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 			if err := cliCtx.EnsureAccountExists(); err != nil {
 				return err
 			}
